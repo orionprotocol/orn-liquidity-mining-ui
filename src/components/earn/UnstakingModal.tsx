0693552a5654682246278dde02cf9c076908bf15
tolya-yanot <tolyayanot@gmail.com>
2020-11-18 20:45:04 +0300

    withdraw gas limit 500k

--- src/components/earn/UnstakingModal.tsx
+++ src/components/earn/UnstakingModal.tsx
@@ -44,7 +44,7 @@ export default function UnstakingModal({ isOpen, onDismiss, stakingInfo }: Staki
     if (stakingContract && stakingInfo?.stakedAmount) {
       setAttempting(true)
       await stakingContract
-        .exit({ gasLimit: 300000 })
+        .exit({ gasLimit: 500000 })
         .then((response: TransactionResponse) => {
           addTransaction(response, {
             summary: `Withdraw deposited liquidity`
